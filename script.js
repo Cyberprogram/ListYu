3ee85cc603e2633f908dabd40d4aaef24b850dac
hugi <hugo.cossec@gmail.com>
2022-10-26 21:24:18 +0200

    ChangementEffectuer

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 // RECUPERER L'API Pronote PUIS PRENDRE les infos
+// ENT "Toutatice" (Rennes) (CAS : toutatice, URL : "www.toutatice.fr")
+const pronote = require("pronote-lib");
 
-const fetchPlaylist = async () => {
-  infos =
-    await fetch(`http://192.168.42.182:5500/?username=H.cossec1&password=Maison99100&link=https://0220058u.pronote.toutatice.fr/pronote/eleve.html?identifiant=
-  `)
-      .then((response) => {
-        return response.json();
+let url = "https://0220058u.pronote.toutatice.fr/pronote";
+let cas = "ac-Rennes"; //Use none bypass CAS auth.
+
+let user = new pronote.User(
+  { username: "H.cossec1", password: "Maison99100", url, cas },
+  function () {
+    user
+      .get_marks()
+      .then(function (res) {
+        console.log(res);
       })
-      .then(function once(result) {
-        console.log(result);
+      .catch(function (err) {
+        console.error(err);
       });
-};
+  }
+);
